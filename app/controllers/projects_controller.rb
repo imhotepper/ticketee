6167981372614a88bb93783ce5ebc5383d786cad
Dragosh <stefanescu.Dragos@gmail.com>
2012-12-16 20:01:59 +0200

    Title functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -20,5 +20,6 @@ class ProjectsController < ApplicationController
   
   def show
     @project = Project.find(params[:id])
+    
   end
 end
--- spec/integration/creating_projects_spec.rb
+++ spec/integration/creating_projects_spec.rb
@@ -3,12 +3,22 @@ require 'spec_helper'
 
 feature 'Creating Projects' do
   scenario 'can create a project' do
+    
+    project_name = "First Project"
+    
     visit '/'
     click_link 'New Project'
-    fill_in 'Name', :with => "First project"
+    fill_in 'Name', :with => project_name
     fill_in 'Description' , :with => "First project description"
     click_button 'Create Project'
     page.should have_content("Project has been created")
+    
+    project = Project.find_by_name project_name
+    page.current_url.should == project_url(project)
+    
+    title = "#{project_name} - Projects - Ticketee"
+    find("title").should have_content(title )
+    
   end
   
 end
